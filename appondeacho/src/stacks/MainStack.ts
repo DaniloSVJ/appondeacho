4ab7e3c5f257f8a5c4e9994f7ad293f1dfe53c2a
Danilo <DaniloSVJ>
2024-01-24 12:42:33 -0300

    first commit

---
+++ appondeacho/src/stacks/MainStack.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import {  } from '@react-navigation/stack';
+
+
+
+const StackNavigator = async () => {
+  const Stackn =  await createStackNavigator();
+  const 
+  return (
+    <Stackn.Navigator>
+      {/* Adicione suas telas ou outros componentes de navegação aqui */}
+    </Stackn.Navigator>
+  );
+};
+
+export default StackNavigator;
